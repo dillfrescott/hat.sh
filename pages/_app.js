8a9a317fc6c904a23e297ff5daa5694a0d42dfd7
Cross <cross@dill.moe>
2024-09-17 18:44:57 -0400

    Remove Umami code

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,6 @@ function MyApp({ Component, pageProps }) {
           {t("sub_title")}
         </title>
         <link rel="icon" href="/favicon.ico" />
-        <script defer src="https://umami.dill.moe/script.js" data-website-id="f8febd21-6a47-42ea-9c13-6c18c2f448cf"></script>
 
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
